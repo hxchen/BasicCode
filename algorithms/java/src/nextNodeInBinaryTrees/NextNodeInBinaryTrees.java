575bc56c593319e5a4be0edf427d5a8a58d47829
Wayne-MBP <506110230@qq.com>
2018-05-24 00:31:29 +0800

    :sparkles:add Tests for NextNodeInBinaryTrees

--- algorithms/java/src/nextNodeInBinaryTrees/NextNodeInBinaryTrees.java
+++ algorithms/java/src/nextNodeInBinaryTrees/NextNodeInBinaryTrees.java
@@ -1,6 +1,7 @@
 package nextNodeInBinaryTrees;
 
 import constructBinaryTree.ConstructBinaryTree;
+import org.junit.Test;
 
 /**
  * 二叉树的下一个结点
@@ -15,8 +16,66 @@ public class NextNodeInBinaryTrees {
         BinaryTreeNode mLeft;
         BinaryTreeNode mRight;
         BinaryTreeNode mParent;
+
+        public BinaryTreeNode(int value){
+            this.value = value;
+        }
+    }
+    /**
+     * 创建树
+     * @param parent
+     * @param left
+     * @param right
+     */
+    public void ConnectTreeNodes(BinaryTreeNode parent, BinaryTreeNode left, BinaryTreeNode right){
+        if(parent != null){
+            parent.mLeft = left;
+            parent.mRight = right;
+
+            if (left != null){
+                left.mParent = parent;
+            }
+            if(right != null){
+                right.mParent = parent;
+            }
+        }
     }
 
+    /**
+     * 打印树节点
+     * @param node
+     */
+    public void PrintTreeNode(BinaryTreeNode node){
+        if(node != null){
+            System.out.println("value of this node is "+node.value);
+            if(node.mLeft != null){
+                System.out.println("value of its left child is "+node.mLeft.value);
+            }else{
+                System.out.println("value of its left child is null");
+            }
+            if(node.mRight != null){
+                System.out.println("value of its right child is "+node.mRight.value);
+            }else{
+                System.out.println("value of its right child is null");
+            }
+        }
+    }
+
+    /**
+     * 完整打印树
+     * @param root
+     */
+    public void PrintTree(BinaryTreeNode root){
+        PrintTreeNode(root);
+        if(root != null){
+            if(root.mLeft != null){
+                PrintTree(root.mLeft);
+            }
+            if(root.mRight != null){
+                PrintTree(root.mRight);
+            }
+        }
+    }
     public BinaryTreeNode getNext(BinaryTreeNode node){
         if(node == null){
             return null;
@@ -43,4 +102,93 @@ public class NextNodeInBinaryTrees {
         }
         return next;
     }
+
+    // ====================测试代码====================
+    public void Test(String name, BinaryTreeNode node, BinaryTreeNode expected){
+        if(null != name){
+            System.out.print(name+"\t");
+        }
+        BinaryTreeNode next = getNext(node);
+        if (next == expected){
+            System.out.println("Passed.");
+        }else{
+            System.out.println("Failed.");
+        }
+    }
+    
+    //            8
+    //        6      10
+    //       5 7    9  11
+    // {5,6,7,8,9,10,11}
+    @Test
+    public void Test1_7(){
+        BinaryTreeNode node8 = new BinaryTreeNode(8);
+        BinaryTreeNode node6 = new BinaryTreeNode(6);
+        BinaryTreeNode node10 = new BinaryTreeNode(10);
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+        BinaryTreeNode node7 = new BinaryTreeNode(7);
+        BinaryTreeNode node9 = new BinaryTreeNode(9);
+        BinaryTreeNode node11 = new BinaryTreeNode(11);
+
+        ConnectTreeNodes(node8, node6, node10);
+        ConnectTreeNodes(node6, node5, node7);
+        ConnectTreeNodes(node10, node9, node11);
+
+       Test("Test1", node8, node9);
+       Test("Test2", node6, node7);
+       Test("Test3", node10, node11);
+       Test("Test4", node5, node6);
+       Test("Test5", node7, node8);
+       Test("Test6", node9, node10);
+       Test("Test7", node11, null);
+    }
+
+    //            5
+    //          4
+    //        3
+    //      2
+    //{2,3,4,5}
+    @Test
+    public void Test8_11(){
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+        BinaryTreeNode node4 = new BinaryTreeNode(4);
+        BinaryTreeNode node3 = new BinaryTreeNode(3);
+        BinaryTreeNode node2 = new BinaryTreeNode(2);
+        ConnectTreeNodes(node5, node4, null);
+        ConnectTreeNodes(node4, node3, null);
+        ConnectTreeNodes(node3, node2, null);
+        Test("Test8", node5, null);
+        Test("Test9", node4, node5);
+        Test("Test10", node3, node4);
+        Test("Test11", node2, node3);
+    }
+
+    //        2
+    //         3
+    //          4
+    //           5
+    //{2,3,4,5}
+    @Test
+    public void Test12_15(){
+        BinaryTreeNode node2 = new BinaryTreeNode(2);
+        BinaryTreeNode node3 = new BinaryTreeNode(3);
+        BinaryTreeNode node4 = new BinaryTreeNode(4);
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+
+
+        ConnectTreeNodes(node2, null, node3);
+        ConnectTreeNodes(node3, null, node4);
+        ConnectTreeNodes(node4, null, node5);
+        Test("Test12", node5, null);
+        Test("Test13", node4, node5);
+        Test("Test14", node3, node4);
+        Test("Test15", node2, node3);
+    }
+
+    @Test
+    public void Test16(){
+        BinaryTreeNode node5 = new BinaryTreeNode(5);
+        Test("Test16", node5, null);
+    }
+
 }
