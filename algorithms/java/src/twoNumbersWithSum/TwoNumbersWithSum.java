9f4d789ea73c3b57b8283dc7719bf46be2baed48
Wayne-MBP <506110230@qq.com>
2018-08-16 22:38:58 +0800

    update dir

--- algorithms/java/src/twoNumbersWithSum/TwoNumbersWithSum.java
+++ algorithms/java/src/twoNumbersWithSum/TwoNumbersWithSum.java
@@ -3,7 +3,7 @@ package twoNumbersWithSum;
 import org.junit.Test;
 
 /**
- *和为s的两个数字
+ * 和为s的两个数字
  * 题目：输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们
  * 的和正好是s。如果有多对数字的和等于s，输出任意一对即可。
  * Created by hxchen on 2018/8/16.
@@ -20,7 +20,7 @@ public class TwoNumbersWithSum {
         System.out.print(", sum is :"+sum);
         int first = 0, second = numbers.length - 1;
         while (first < second){
-            
+
             if (numbers[first] + numbers[second] == sum){
                 System.out.println("  result====find! first = "+numbers[first]+", second = "+numbers[second]);
                 return;
