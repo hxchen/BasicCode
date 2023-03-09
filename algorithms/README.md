203a626d0dd2fd85d2f44bbb7977a0fb562f0d51
hxchen <helloworld.vip@gmail.com>
2023-03-09 21:41:06 +0800

    有向图路径：所有可能的路径

--- algorithms/README.md
+++ algorithms/README.md
@@ -69,10 +69,11 @@
 - [78. 乘积小于 K 的子数组(713)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/subarrayProductLessThanK/Solution.java)
 - [79. 长度最小的子数组(209)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/miniumSizeSubArraySum/Solution.java)
 # 广度/深度优先搜索
-解题思路：变量保存访问信息。深度或者广度优先搜索标记。
+解题思路：变量保存访问信息。深度(递归或者栈)或者广度(队列)优先搜索标记。
 - [80. 岛屿数量(200)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/numberOfIslands/Solution.java)
 - [81. 省份数量(547)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/numberOfProvinces/Solution.java)
 - [82. 填充每个节点的下一个右侧节点指针 II(117)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/populatingNextRightPointersInEachNode/Solution.java)
 - [83. 另一棵树的子树(572)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/subtreeOfAnotherTree/Solution.java)
 - [84. 二进制矩阵中的最短路径(1091)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/shortestPathBinaryMatrix/Solution.java)
 - [85. 被围绕的区域(130)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/surroundedRegions/Solution.java)
+- [86. 所有可能的路径(797)](https://github.com/hxchen/BasicCode/blob/master/algorithms/src/main/java/allPathsFromSourceToTarget/Solution.java)
---
+++ algorithms/src/main/java/allPathsFromSourceToTarget/Solution.java
@@ -0,0 +1,80 @@
+package allPathsFromSourceToTarget;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * 给你一个有 n 个节点的 有向无环图（DAG），请你找出所有从节点 0 到节点 n-1 的路径并输出（不要求按特定顺序）
+ *  graph[i] 是一个从节点 i 可以访问的所有节点的列表（即从节点 i 到节点 graph[i][j]存在一条有向边）。
+ *
+ * 示例1
+ * 输入：graph = [[1,2],[3],[3],[]]
+ * 输出：[[0,1,3],[0,2,3]]
+ * 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+ *
+ * 示例2
+ * 输入：graph = [[4,3,1],[3,2,4],[3],[4],[]]
+ * 输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+ *
+ */
+public class Solution {
+    /**
+     * 深度优先搜索
+     * @param graph
+     * @return
+     */
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> paths = new ArrayList<>();
+        // 双端队列
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.offerLast(0);
+        dfs(graph, 0, graph.length - 1, paths, stack);
+        return paths;
+    }
+
+    /**
+     * 深度遍历
+     * @param graph
+     * @param point 当前处理到的下标
+     * @param end   目标下标
+     * @param paths
+     * @param stack
+     */
+    public void dfs(int[][] graph, int point, int end, List<List<Integer>> paths, Deque<Integer> stack){
+        if (point == end){
+            // 双端队列可以直接初始化ArrayList
+            paths.add(new ArrayList<>(stack));
+            return;
+        }
+        for (int i : graph[point]) {
+            stack.offerLast(i);
+            dfs(graph, i, end, paths, stack);
+            stack.pollLast();
+        }
+
+    }
+
+    @Test
+    public void Test1(){
+        int[][] graph = {
+                {1,2},
+                {3},
+                {3},
+                {}
+        };
+        System.out.println(allPathsSourceTarget(graph));
+    }
+
+    @Test
+    public void Test2(){
+        int[][] graph = {
+                {4, 3, 1},
+                {3, 2, 4},
+                {3},
+                {4},
+                {}
+        };
+        System.out.println(allPathsSourceTarget(graph));
+    }
+}
