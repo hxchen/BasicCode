5c602f665b8ab56f4d0345c61556f08df5e4a4e4
hxchen <helloworld.vip@gmail.com>
2023-04-23 21:59:45 +0800

    修改名字和配置

--- rocketmq/src/main/java/com/a3fun/rocketmq/RocketMQApplication.java
+++ rocketmq/src/main/java/com/a3fun/rocketmq/RocketMQApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 1. 启动RocketMQServer：nohup sh bin/mqnamesrv &
+ * 2. 查看状态：tail -f ~/logs/rocketmqlogs/namesrv.log
+ * 3. 启动Broker：nohup sh bin/mqbroker -n localhost:9876 --enable-proxy &
+ * 4. 查看Broker：tail -f ~/logs/rocketmqlogs/proxy.log
+ * 关闭
+ * 3. sh bin/mqshutdown broker
+ * 4. sh bin/mqshutdown namesrv
+ *
  * @author haixiangchen
  */
 @SpringBootApplication
