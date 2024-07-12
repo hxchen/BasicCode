8891fd75baea56c2ed7330026940945190bbb001
hxchen <helloworld.vip@gmail.com>
2024-07-12 18:02:43 +0800

    合并有序链表，迭代+递归

--- algorithms/src/main/java/mergeSortedLists/MergeSortedLists.java
+++ algorithms/src/main/java/mergeSortedLists/MergeSortedLists.java
@@ -27,6 +27,28 @@ public class MergeSortedLists {
 
         return listNode;
     }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(list1 != null && list2 != null) {
+            if(list1.value <= list2.value) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if(list1 == null) {
+            cur.next = list2;
+        } else {
+            cur.next = list1;
+        }
+        return dummy.next;
+    }
 
     @Test
     public void test1() {
@@ -95,5 +117,28 @@ public class MergeSortedLists {
         System.out.println("合并完链表:");
         ListNode.PrintNode(merge);
 
+    }
+
+    @Test
+    public void test5() {
+        ListNode node1 = new ListNode(1);
+        ListNode node3 = new ListNode(3);
+        ListNode node5 = new ListNode(5);
+        ListNode.ConnectListNode(node1, node3);
+        ListNode.ConnectListNode(node3, node5);
+
+        ListNode node2 = new ListNode(2);
+        ListNode node4 = new ListNode(4);
+        ListNode node6 = new ListNode(6);
+        ListNode.ConnectListNode(node2, node4);
+        ListNode.ConnectListNode(node4, node6);
+
+        System.out.println("合并前链表1:");
+        ListNode.PrintNode(node1);
+        System.out.println("合并前链表2:");
+        ListNode.PrintNode(node2);
+        ListNode merge = MergeSortedLists.mergeTwoLists(node1, node2);
+        System.out.println("合并完链表:");
+        ListNode.PrintNode(merge);
     }
 }
